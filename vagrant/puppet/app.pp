f380315b98d940d694d48c73476b23a04d565036
phiamo <phiamo@googlemail.com>
2013-04-22 20:20:53 +0200

    Updated vagrant with composer and made it more failsafe

--- vagrant/puppet/app.pp
+++ vagrant/puppet/app.pp
@@ -2,6 +2,7 @@ class { 'apt':
   always_apt_update    => true
 }
 Exec { path => ['/usr/local/bin', '/opt/local/bin', '/usr/bin', '/usr/sbin', '/bin', '/sbin'], logoutput => true }
+
 Package { require => Exec['apt_update'], }
 
 $webserverService = $webserver ? {
@@ -38,6 +39,12 @@ class { "java": }
 class { "nodejs": }
 
 package { 'less':
-  ensure   => '1.3.0',
+  ensure   => '1.3.3',
   provider => 'npm',
+}
+class {"composer":
+  target_dir      => '/usr/local/bin',
+  composer_file   => 'composer',
+  download_method => 'curl', # download methods are curl or wget
+  logoutput       => false
 }
