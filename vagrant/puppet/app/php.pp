121cfe42d361e76cde6101d244f73e76c69a6174
phiamo <phiamo@googlemail.com>
2013-04-29 19:41:10 +0200

    Updated vagrant setup to run more flawlessly

--- vagrant/puppet/app/php.pp
+++ vagrant/puppet/app/php.pp
@@ -1,12 +1,7 @@
 class app::php {
-    package {["php5", "php5-cli", "php5-dev", "php-apc", "php5-mysql"]:
+    package {["php5", "php5-dev", "php-apc", "php5-mysql", "php5-suhosin", "php5-cli"]:
         ensure => present,
         notify => Service[$webserverService],
-    }
-
-    exec {"clear-symfony-cache":
-        require => Package["php5-cli"],
-        command => "/bin/bash -c 'cd /srv/www/vhosts/$vhost.dev && /usr/bin/php app/console cache:clear --env=dev && /usr/bin/php app/console cache:clear --env=prod'",
     }
 
     if 'nginx' == $webserver {
